2dfef407749af9f89671278982c28a812e702daf
MaxDLink <max.durham.link@gmail.com>
2023-04-06 18:36:04 -0500

    added struct to represent application

--- hw3.cpp
+++ hw3.cpp
@@ -4,6 +4,14 @@
 
 using namespace std;
 
+// Define a struct to represent an applicant
+struct Applicant {
+    int id;             // unique identifier of the applicant
+    float wpm;          // words per minute
+    float ipm;          // improper words per minute
+    bool eligible;      // whether the applicant is eligible or not
+};
+
 vector<int> BestApplicants(const vector<pair<float, float> >& applicants)
 {
    vector<int> res;
